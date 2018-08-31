78d69996a06414d5b7d86030b4ef008489244d84
federico <federico.naldini3@studio.unibo.it>
2018-08-31 15:29:27 +0200

    Added Scaladoc and removed bad handler

--- src/main/scala/VertxLoader/VertxFile.scala
+++ src/main/scala/VertxLoader/VertxFile.scala
@@ -2,13 +2,21 @@ package VertxLoader
 
 import io.vertx.core.{AsyncResult, Handler, Vertx}
 
-
+/**
+  * Trait to describe a generic VertxFile.
+  */
 trait VertxFile {
-
   def filePath:String
-  def openFile():Unit
+
+  /**
+    * Open the file at a specified path and process it.
+    */
+  def computeFile():Unit
 }
 
+/**
+  * Companion Object for the trait VertxFile.
+  */
 object VertxFile {
 
   private[this] val noNesting = 0
@@ -16,42 +24,58 @@ object VertxFile {
   import java.io._
 
 
+  /**
+    * Apply method for build a VertxFile object without a nesting level specified.
+    * @param filePath the path of the file to open
+    * @return a Optional containing a object a VertxFile if the file exists, None otherwise.
+    */
   def apply(filePath:String): Option[VertxFile] = this(filePath, noNesting)
 
+  /**
+    * Apply method for building a vertxfile with a specified nestingLevel
+    * @param filePath the path of the file to open
+    * @param nestingLevel nesting level to reach in subfolders(used if the file is a folder)
+    * @return a Optional containing a object a VertxFile if the file exists, None otherwise.
+    */
   def apply(filePath:String, nestingLevel:Int): Option[VertxFile] = filePath match {
       case VertxFile(file) if file.isDirectory => Some(new VertxFolder(filePath, nestingLevel))
       case VertxFile(file) => Some(new VertxDocument(filePath))
       case _ => None
   }
 
+  /**
+    * Unapply method to extract a file from a path.
+    * @param filePath the path of the file to open
+    * @return A file if the file exists, None otherwise
+    */
   def unapply(filePath: String): Option[File] = {val file = new File(filePath); file match {
-    case file:File if file.exists() => Some(file)
+    case file: File if file.exists() => Some(file)
     case _ => None
   }}
 
 
-
-
-
+  /**
+    * This class represent a document to compute.
+    * @param filePath the path of the file to open.
+    */
   private class VertxDocument(override val filePath: String) extends VertxFile {
-    override def openFile(): Unit = ???
+    override def computeFile(): Unit = ???
   }
 
+  /**
+    * This class represent a folder to compute.
+    * @param filePath the path of the file to open.
+    */
   private class VertxFolder(override val filePath: String, val nestingLevel:Int) extends VertxFile {
-
-
-    val handler:Handler[AsyncResult[List[String]]] = (result:AsyncResult[List[String]]) => result match {
-      case AsyncResult if result.succeeded() => result.result().foreach(path => VertxFile(path, nestingLevel -1).get.openFile)
+    /*val handler = (result:Handler[AsyncResult[List[String]]]) => result match {
+      case result: Handler if result.succeeded() => result.result().foreach(path => VertxFile(path, nestingLevel -1).get.computeFile)
       case _ => result.cause()
+    }*/
+
+    override def computeFile(): Unit = {
+      //Vertx.vertx.fileSystem.readDir(filePath, handler)
     }
-
-    override def openFile(): Unit = {
-      Vertx.vertx.fileSystem.readDir(filePath, handler)
-
-    }
-
   }
-
 }
 
 
