8761a3d3ad5634b77407e67c7d5ef28e6880f515
federico <federico.naldini3@studio.unibo.it>
2018-09-05 17:47:04 +0200

    Removed redundant code

--- src/main/scala/VertxLoader/VertxFile.scala
+++ src/main/scala/VertxLoader/VertxFile.scala
@@ -21,13 +21,14 @@ sealed trait VertxFile {
 object VertxFile {
 
   private[this] val loader: Vertx = Vertx.vertx()
+
   import java.io._
 
 
   /**
     * Apply method for building a vertxfile with a specified nestingLevel
     *
-    * @param filePath     the path of the file to open
+    * @param filePath the path of the file to open
     * @return a Optional containing a object a VertxFile if the file exists, None otherwise.
     */
   def apply(filePath: String): Option[VertxFile] = filePath match {
@@ -51,28 +52,27 @@ object VertxFile {
   }
 
 
-  private def getSubFolderDocuments(filePath:String, nestingLevel:Int):Future[List[VertxFile]] =
+  private def getSubFolderDocuments(filePath: String, nestingLevel: Int): Future[List[VertxFile]] =
     loader.fileSystem()
       .readDirFuture(filePath)
-      .map(buffer => Future.sequence(buffer.toStream.map(path => buildFileList(path, nestingLevel-1))))
+      .map(buffer => Future.sequence(buffer.toStream.map(path => buildFileList(path, nestingLevel - 1))))
       .flatten
       .map(s => s.flatten.toList)
 
 
-
-
-
-
-  private def getFolderDocuments(folderPath:VertxFolder):Future[List[VertxFile]] = loader.fileSystem()
+  private def getFolderDocuments(folderPath: VertxFolder): Future[List[VertxFile]] = loader.fileSystem()
     .readDirFuture(folderPath.filePath).map(buffer => buffer.toStream.map(s => VertxFile(s).get).toList)
 
-  private[this] def buildFileList(root:String, nestingLevel:Int):Future[List[VertxFile]] = VertxFile(root).get match {
-      case document: VertxDocument =>  Future{List(document)}
-      case folder:VertxFolder if nestingLevel == 0 => getFolderDocuments(folder)
-      case folder: VertxFolder if nestingLevel > 0 => getSubFolderDocuments(root, nestingLevel)
-      case _ => Promise.failed(new IllegalStateException("Error in opening "+root)).future
+  private[this] def buildFileList(root: String, nestingLevel: Int): Future[List[VertxFile]] = VertxFile(root).get match {
+    case document: VertxDocument => Future {
+      List(document)
+    }
+    case folder: VertxFolder if nestingLevel == 0 => getFolderDocuments(folder)
+    case folder: VertxFolder if nestingLevel > 0 => getSubFolderDocuments(root, nestingLevel)
+    case _ => Promise.failed(new IllegalStateException("Error in opening " + root)).future
   }
-  def scanAndApply[A](filePath:String, nestingLevel:Int, strategy: List[VertxFile] => A):Future[A] =
+
+  def scanAndApply[A](filePath: String, nestingLevel: Int, strategy: List[VertxFile] => A): Future[A] =
     buildFileList(filePath, nestingLevel).map(strategy)
 
   /**
@@ -89,34 +89,7 @@ object VertxFile {
     *
     * @param filePath the path of the file to open.
     */
-  private[VertxLoader] case class VertxFolder(override val filePath: String) extends VertxFile/* {
-
-    /**
-      * Import used to convert Java collection into scala ones.
-      */
-
-    import scala.collection.JavaConverters._
-
-
-    private[this] val filterDocument: VertxFile => Boolean = (file: VertxFile) => file.isInstanceOf[VertxDocument]
-
-    private[this] val mapToVertxFile: String => VertxFile = (filePath: String) => VertxFile(filePath).get
-
-    private[this] val handler = (result: AsyncResult[java.util.List[String]]) => result match {
-
-      case result: AsyncResult[java.util.List[String]] if result.succeeded() && nestingLevel > 0 =>
-        result.result().forEach(path => VertxFile(path, nestingLevel - 1).get.computeFile())
-
-      case result: AsyncResult[java.util.List[String]] if result.succeeded() =>
-        result.result().asScala.toStream.map(mapToVertxFile).filter(filterDocument).foreach(f => f.computeFile())
-      case _ => println(result.cause())
-    }
-
-    override def computeFile(): Unit = {
-      loader.fileSystem.readDir(filePath, handler)
-    }
-  }*/
+  private[VertxLoader] case class VertxFolder(override val filePath: String) extends VertxFile
 
 }
-
 
