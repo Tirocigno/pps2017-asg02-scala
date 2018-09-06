da6e08dc90330cc67ba2c7ff533e43025aff69d9
federico <federico.naldini3@studio.unibo.it>
2018-09-06 17:50:31 +0200

    Implemented StatisticsStorage object.

---
+++ src/main/scala/utilities/StatisticsStorage.scala
@@ -0,0 +1,32 @@
+package utilities
+
+object StatisticsStorage {
+
+  private var scannedFileMap: Map[String, Long] = scala.collection.immutable.HashMap()
+
+  /**
+    * Add a new file to the map.
+    *
+    * @param filePath the path of the scanned file.
+    * @param value    the value of the file calculated.
+    */
+  def addFile(filePath: String, value: Long): Unit =
+    scannedFileMap = scannedFileMap + (filePath -> value)
+
+
+  /**
+    * Retrieve the number of file which match at least once.
+    *
+    * @return an Integer containing the value.
+    */
+  def getNumberOfFileMatching(): Int = scannedFileMap.filter(_._2 > 0).keys size
+
+  /**
+    * Retrieve the number of total matches in the storage.
+    *
+    * @return a Long containing the value.
+    */
+  def getTotalMatching(): Long = scannedFileMap.filter(_._2 > 0).values sum
+
+
+}
