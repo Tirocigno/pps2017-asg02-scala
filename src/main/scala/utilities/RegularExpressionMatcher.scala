ede427f0c7e6afcf5cd1f53404840445337be17f
federico <federico.naldini3@studio.unibo.it>
2018-09-07 17:58:34 +0200

    Implemented matchRegularExpression method.

--- src/main/scala/utilities/RegularExpressionMatcher.scala
+++ src/main/scala/utilities/RegularExpressionMatcher.scala
@@ -1,13 +1,32 @@
 package utilities
 
-import VertxLoader.VertxFile
+import VertxLoader.VertxFile.VertxDocument
+import VertxLoader._
+import utilities.StatisticsStorage._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.matching.Regex
+import scala.util.{Failure, Success}
 
 object RegularExpressionMatcher {
 
-  def matchRegularExpression(fileList: List[VertxFile], regularExpression: String): Unit = ???
+  def matchRegularExpression(fileList: List[VertxFile], regularExpression: String): Unit =
+    fileList.filter(_.isInstanceOf[VertxDocument]).map(_.asInstanceOf[VertxDocument]) foreach (d => readVertxFile(d)
+      .map(_.words)
+      .map(_.filter(checkRegExp(_, regularExpression.r)))
+      .map(_.size)
+      .onComplete({
+        case Success(occurences) => addFile(d.filePath, occurences)
+        case Failure(exception) => throw new IllegalStateException(exception)
+      }))
 
   implicit class TextString(text: String) {
     def words(): Iterable[String] = text.split("\\W+").toIterable
+  }
+
+  implicit def checkRegExp(word: String, regExp: Regex): Boolean = word match {
+    case regExp(_) => true
+    case _ => false
   }
 
 }
--- src/test/scala/utilities/RegularExpressionMatcherTest.scala
+++ src/test/scala/utilities/RegularExpressionMatcherTest.scala
@@ -1,7 +1,18 @@
 package utilities
 
 import org.scalatest.FunSuite
+import utilities.RegularExpressionMatcher._
 
 class RegularExpressionMatcherTest extends FunSuite {
+
+  test("Split test on a string") {
+    val text = "Sul tagliere l'aglio taglia, non tagliare la tovaglia!"
+    assert(text.words().size == 9)
+  }
+
+  test("Split test on multiple lines string") {
+    val text = "Nel cammin di nostra vita \n , mi ritrovai per una selva oscura\n"
+    assert(text.words().size == 11)
+  }
 
 }
